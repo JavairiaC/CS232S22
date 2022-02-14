b53902dee74b675ee06d5bf3a3696c93c44ace3e
JavairiaC <78161890+JavairiaC@users.noreply.github.com>
2022-02-13 19:34:03 -0500

    Add files via upload

---
+++ assign2/task2/arith.c
@@ -0,0 +1,17 @@
+/*  Example: C program to find area of a circle */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, b;
+
+  printf("Enter radius (in mm):\n");
+  scanf("%f", &r);
+
+  a = PI * (r/25.4) * (r/25.4);
+  b = 2 * PI * (r/25.4);
+  printf("Circle's area is %3.2f (sq in).\n", a);
+  printf("Its circumference is %3.2f (in).\n", b);
+}
